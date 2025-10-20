043a4c4608ad1838c884364ae0f40be7a60c0a6d
Alper Çıtak <citak.alper@gmail.com>
2025-10-20 14:48:11 +0200

    handle voice selector

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <div class="voice-selector">
       <n-select v-model:value="language" :options="languageOptions" filterable />
-      <n-select v-model:value="selectedVoice" :options="computedVoiceOptions" />
+      <n-select v-model:value="voice" :options="voiceOptions" />
     </div>
     <n-input v-model:value="text" type="textarea" />
     <n-button @click="speak" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">🔊 Speak</n-button>
@@ -10,46 +10,49 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 import { NButton, NInput, NSelect } from 'naive-ui';
 import type { SelectOptions } from './types';
 
 const voices = ref<Array<SpeechSynthesisVoice>>([]);
 const language = ref<string>('');
-const selectedVoice = ref<string>('');
+const voice = ref<string>('');
 const text = ref<string>('Hello, welcome to Vue text to speech!');
 
 const uniqueLanguageOptions = computed<Array<string>>(() =>
-  Array.from(new Set(voices.value.map(({ lang }) => lang))).sort()
+  Array.from(new Set(voices.value.map(({ lang }) => lang))).toSorted()
 );
 const languageOptions = computed<SelectOptions>(() =>
   uniqueLanguageOptions.value.map((lang) => ({ label: lang, value: lang }))
 );
-const computedVoiceOptions = computed<Array<{ label: string; value: string }>>(() =>
-  voices.value.map((voice) => ({
-    label: `${voice.name} (${voice.lang})`,
-    value: voice.name,
-  }))
+const voiceOptions = computed<SelectOptions>(() =>
+  voices.value
+    .filter(({ lang }) => lang === language.value)
+    .map(({ name }) => ({ label: name, value: name }))
+    .toSorted((a, b) => a.label.localeCompare(b.label))
 );
 
 window.speechSynthesis.onvoiceschanged = () => {
   voices.value = window.speechSynthesis.getVoices();
-  if (voices.value.length && !selectedVoice.value) {
-    selectedVoice.value = voices.value[0].name;
+  if (voices.value.length) {
     language.value = voices.value[0].lang;
   }
 };
+
+watch(language, () => {
+  voice.value = voiceOptions.value[0].value;
+});
 
 function speak() {
   if (!window.speechSynthesis) {
     alert('Sorry, your browser does not support Text-to-Speech.');
     return;
   }
+  speechSynthesis.cancel();
   const utterance = new SpeechSynthesisUtterance(text.value);
-  const voice = voices.value.find((v) => v.name === selectedVoice.value);
-  utterance.lang = 'en-US'; // Change to 'fr-FR', 'es-ES', etc. if needed
-  if (voice) {
-    utterance.voice = voice;
+  const selectedVoice = voices.value.find((v) => v.name === voice.value);
+  if (selectedVoice) {
+    utterance.voice = selectedVoice;
   }
   speechSynthesis.speak(utterance);
 }
